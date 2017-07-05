4da165de233aae560b47999c1292d3e8f79fe834
chandu0101 <chandu.csu2010@gmail.com>
2017-07-05 07:39:04 +0530

    skip abstract classes while scanning

--- src/main/scala/scalajsjest/JestMacro.scala
+++ src/main/scala/scalajsjest/JestMacro.scala
@@ -49,13 +49,14 @@ private[scalajsjest] object JestMacro {
         .decls
         .foreach(s => {
           if (s.isPackage) allTestClasses(s.fullName)
-          else if (s.isClass && s.asClass.toType != typeOf[JestSuite] && s.asClass.toType <:< typeOf[
-                     JestSuite]) {
+          else if (s.isClass && !s.isAbstract && s.asClass.toType != typeOf[
+                     JestSuite] && s.asClass.toType <:< typeOf[JestSuite]) {
             result :+= q"new ${s.asClass.toType}"
           }
         })
     }
     allTestClasses(rootPackage)
+    println(s"CLasses : $result")
     q"""
         ..$result
       """
